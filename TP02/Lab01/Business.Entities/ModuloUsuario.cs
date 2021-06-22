3d1c059ad20d15e43f636636e2a8e53f48ae1cd1
Emiliano Francisco Gamba <emilianofranciscogamba@outlook.com>
2021-06-22 15:53:41 -0300

    Avance TP02

---
+++ TP02/Lab01/Business.Entities/ModuloUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Entities
+{
+    public class ModuloUsuario:BusinessEntity
+    {
+        private int _IdUsuario;
+        private int _IdModulo;
+        private bool _PermiteAlta;
+        private bool _PermiteBaja;
+        private bool _PermiteModificacion;
+        private bool _PermiteConsulta;
+
+        public int IdUsuario { get => _IdUsuario; set => _IdUsuario = value; }
+        public int IdModulo { get => _IdModulo; set => _IdModulo = value; }
+        public bool PermiteAlta { get => _PermiteAlta; set => _PermiteAlta = value; }
+        public bool PermiteBaja { get => _PermiteBaja; set => _PermiteBaja = value; }
+        public bool PermiteModificacion { get => _PermiteModificacion; set => _PermiteModificacion = value; }
+        public bool PermiteConsulta { get => _PermiteConsulta; set => _PermiteConsulta = value; }
+    }
+}
