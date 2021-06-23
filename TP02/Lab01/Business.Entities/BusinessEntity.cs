6fee583952b97315cbb90f9e5c7fac6e617f6377
Emiliano Francisco Gamba <emilianofranciscogamba@outlook.com>
2021-06-22 22:44:39 -0300

    Diagrama de clases

--- TP02/Lab01/Business.Entities/BusinessEntity.cs
+++ TP02/Lab01/Business.Entities/BusinessEntity.cs
@@ -30,4 +30,6 @@ namespace Business.Entities
             Unmodified
         }
     }
+
+
 }
---
+++ TP02/Lab01/Business.Entities/Enum1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Entities
+{
+    public enum Enum1
+    {
+    }
+}
