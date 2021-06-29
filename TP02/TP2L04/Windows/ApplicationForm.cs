c68eb5b7548b95939857248f2fd7b893393e65bb
Renzo <Renzomilanesi95@hotmail.com>
2021-06-29 10:48:41 -0300

    Hasta por arrancar ejercicio 2

---
+++ TP02/TP2L04/Windows/ApplicationForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows // esto desde un principio tendriamos que haberlo llamado UI.Desktop pero bueno,,, 
+{
+    public partial class ApplicationForm : Form
+    {
+        public ApplicationForm()
+        {
+            InitializeComponent();
+        }
+
+        public enum ModoForm
+        { Alta, Baja, Modificacion, Consulta };
+
+        public ModoForm modo { get; set; }
+
+        public virtual void MapearDeDatos() { }
+        public virtual void MapearADatos() { }
+        public virtual void GuardarCambios() { }
+        public virtual bool Validar() { return false; }
+        public void Notificar(string titulo, string mensaje, MessageBoxButtons botones, MessageBoxIcon icono)
+        {
+            MessageBox.Show(mensaje, titulo, botones, icono);
+        }
+        public void Notificar(string mensaje, MessageBoxButtons botones, MessageBoxIcon icono)
+        {
+            this.Notificar(this.Text, mensaje, botones, icono);
+        }
+
+        
+    }
+
+   
+}
---
+++ TP02/TP2L04/Windows/UsuarioDesktop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace Windows
+{
+    public partial class UsuarioDesktop : Windows.ApplicationForm
+    {
+        public UsuarioDesktop()
+        {
+            InitializeComponent();
+        }
+    }
+}
