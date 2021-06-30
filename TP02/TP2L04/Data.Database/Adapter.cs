a87801031f35f0ffd869cc99cf055bb17a9d7899
Emiliano Francisco Gamba <emilianofranciscogamba@outlook.com>
2021-06-30 11:24:46 -0300

    Se agrego App.config en UI.Desktop y se modificio adapter

--- TP02/TP2L04/Data.Database/Adapter.cs
+++ TP02/TP2L04/Data.Database/Adapter.cs
@@ -2,20 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Data.Database
 {
     public class Adapter
     {
-        //private SqlConnection sqlConnection = new SqlConnection("ConnectionString;");
+        const string consKeyDefaultCnnString = "ConnStringExpress";
+
+        private SqlConnection sqlConn = new SqlConnection("ConnectionString");
 
         protected void OpenConnection()
         {
+            var connectionstring = ConfigurationManager.ConnectionStrings[consKeyDefaultCnnString].ConnectionString;
+            SqlConnection sqlConn = new SqlConnection(connectionstring);
+            sqlConn.Open();
             throw new Exception("Metodo no implementado");
+          
+
+       
         }
 
         protected void CloseConnection()
         {
+            sqlConn = null;
+            sqlConn.Close();
             throw new Exception("Metodo no implementado");
         }
 
---
+++ TP02/TP2L04/Windows (2)/Usuarios.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace Windows
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.dgvUsuarios.DataSource = ul.getAll();
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            dgvUsuarios.AutoGenerateColumns = false;
+            this.Listar();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsbNuevo_Click(object sender, EventArgs e)
+        {
+            UsuarioDesktop formUsuario = new UsuarioDesktop(ApplicationForm.ModoForm.Alta);
+            formUsuario.ShowDialog();
+            this.Listar();
+        }
+
+        //private void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{
+
+        //}
+        // para hacer commit de 12:25
+    }
+}
